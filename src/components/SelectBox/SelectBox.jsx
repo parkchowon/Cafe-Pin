b38b81a76dba6b66f0387763ed6e5824c84a65bd
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-06-20 20:57:48 +0900

    Feat: login 할때 localStorage 토큰저장

--- src/components/SelectBox/SelectBox.jsx
+++ src/components/SelectBox/SelectBox.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { SWITCH_TO_LOGGED_OUT } from '../../redux/slices/isLoggedInSlice';
 import { Item, Wrapper } from './SelectBox.style';
 
 function SelectBox() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn.isLoggedIn);
 
   const handleClick = (e) => {
-    navigate(e.target.id);
+    const path = e.target.id;
+    if (path == '') {
+      dispatch(SWITCH_TO_LOGGED_OUT());
+      localStorage.removeItem('accessToken');
+      navigate(path);
+    }
+    navigate(path);
   };
 
   return (
     <Wrapper>
-      <Item className="first" id="auth/sign-up" onClick={handleClick}>
-        회원가입
-      </Item>
-      <Item className="second" id="auth/login" onClick={handleClick}>
-        로그인
-      </Item>
+      {isLoggedIn ? (
+        <>
+          <Item className="first" id="my-page" onClick={handleClick}>
+            마이페이지
+          </Item>
+          <Item className="second" id="" onClick={handleClick}>
+            로그아웃
+          </Item>
+        </>
+      ) : (
+        <>
+          <Item className="first" id="auth/sign-up" onClick={handleClick}>
+            회원가입
+          </Item>
+          <Item className="second" id="auth/login" onClick={handleClick}>
+            로그인
+          </Item>
+        </>
+      )}
     </Wrapper>
   );
 }
--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SelectBox from '../../SelectBox/SelectBox';
 import CoffeeBean from '../Icon/CoffeeBean';
@@ -7,9 +8,15 @@ import { Logo, ProfileImg, User, Wrapper } from './Header.style';
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const isLoggedIn = useSelector((state) => state.isLoggedIn.isLoggedIn);
+  useEffect(() => {
+    console.log(isLoggedIn);
+  }, []);
+
   const handleProfileClick = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <Wrapper>
       <Logo>
@@ -18,10 +25,18 @@ function Header() {
           <p className="logo-text">Cafe Pin</p>
         </Link>
       </Logo>
-      <User>
-        <p className="user-name">Guest 님</p>
-        <ProfileImg className="user-img" onClick={handleProfileClick}></ProfileImg>
-      </User>
+      {isLoggedIn ? (
+        <User>
+          <p className="user-name">로그인한사람 님</p>
+          <ProfileImg className="user-img" onClick={handleProfileClick}></ProfileImg>
+        </User>
+      ) : (
+        <User>
+          <p className="user-name">Guest 님</p>
+          <ProfileImg className="user-img" onClick={handleProfileClick}></ProfileImg>
+        </User>
+      )}
+
       {isOpen && <SelectBox />}
     </Wrapper>
   );
